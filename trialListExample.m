bcf809e31e3c79410abe427ffb33d762cd0f903f
Lisa <lisa@Lisas-MacBook-Pro-2.local>
2018-03-06 11:21:59 +0100

    fix

--- trialListExample.m
+++ trialListExample.m
@@ -1,4 +1,5 @@
-% Angenommen wir wollen ein Experiment bauen wo Boxen entweder grün oder blau sind und entweder links oder rechts erscheinen 
+% Angenommen wir wollen ein Experiment bauen wo Boxen entweder grün oder 
+% blau sind und entweder links oder rechts erscheinen 
 
 trialN = 20;
 farbenN=2;
@@ -6,11 +7,13 @@ seitenN=2;
 
 
 trials= 1:20;
+
 farbe= repelem([1:farbenN], trialN/farbenN);
+
 seite= repmat([1:seitenN], 1,trialN/seitenN);
 
 trialList = horzcat(farbe', seite');
 rng(1234)
 randIx= randperm(size(trialList,1));
 trialList= trialList(randIx,:);
-trilList= horzcat(trials', trialList)
+trialList= horzcat(trials', trialList)
